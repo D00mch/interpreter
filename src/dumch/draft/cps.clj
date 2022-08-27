97f4802fed067f7f116c63ca07a002d1635e1341
liverm0r <arturdumchev@yandex.ru>
2022-08-27 03:00:10 +0300

    support analyze and tailrec for CPS draft

--- src/dumch/draft/cps.clj
+++ src/dumch/draft/cps.clj
@@ -1,4 +1,6 @@
-(ns dumch.draft.cps)
+(ns dumch.draft.cps )
+
+(set! *warn-on-reflection* false)
 
 ;;; # Env
 
@@ -14,7 +16,9 @@
 
 ;;; # Interpreter in Continuation Passing Style
 
-(defprotocol IEvalCPS (eval-cps [this env k]))
+#_(defprotocol IEvalCPS (eval-cps [this env k]))
+(defprotocol IAnalyze (analyze [sexp]))
+(declare eval-cps)
 
 (defn lookup-env-k [{:keys [symbols outer] :as env} x succ fail]
   (cond 
@@ -22,19 +26,23 @@
     (contains? symbols x) (succ (get symbols x))
     :else (lookup-env-k outer x succ fail)))
 
+(comment 
+  (lookup-env-k top-env 'x identity #(println :fuck!)))
+
+
 (def top-k (fn [r] 
              (println :error r)
              r))
 
-(defn get-args [[arg & args] env k]
-  (if arg
-    (eval-cps arg
-              env
-              (fn [v]
-                (get-args args 
-                          env 
-                          (fn [vs]
-                            (k (cons v vs))))))
+(defn get-args [[arg-f & arg-fs] env k]
+  (if arg-f
+    (arg-f
+      env
+      (fn [v]
+        (get-args arg-fs 
+                  env 
+                  (fn [vs]
+                    (k (cons v vs))))))
     (k '())))
 
 ;(get-args [1 2 '(+ 1 2)] top-env identity)
@@ -42,71 +50,101 @@
 (defrecord Continuation [env k])
 
 (defn eval-seq [[h & tail] env k]
-  (cond tail (eval-cps h 
-                       env 
-                       (fn [_]
-                         (eval-seq tail env k)))
-        h (eval-cps h env k)))
+  (cond tail #(eval-cps h 
+                        env 
+                        (fn [_]
+                          (eval-seq tail env k)))
+        h #(eval-cps h env k)))
 
-(extend-protocol IEvalCPS
-  java.lang.Long
-  (eval-cps [this _ k] (k this))
+(defn analyze-self-eval [sexp]
+  (fn [env k] (k sexp)))
 
-  java.lang.Boolean
-  (eval-cps [this _ k] (k this))
-
-  clojure.lang.Symbol
-  (eval-cps [this env k]
+(defn analyze-lookup [sexp]
+  (fn [env k] 
     (lookup-env-k env 
-                  this
+                  sexp
                   k 
-                  #(top-k (format "%s not bound" this))))
+                  #(top-k (format "%s not bound" sexp)))))
 
-  Continuation
-  (eval-cps [this _ k] (k this))
+(defn analyze-if [[_ test-expr then else]]
+  (let [test-fn (analyze test-expr)
+        then-fn (analyze then)
+        else-fn (analyze else)]
+    (fn [env k] 
+      (test-fn env
+               (fn [b]
+                 (if (boolean? b)
+                   ((if b then-fn else-fn) env k)
+                   (top-k (format "%s not a boolean" b))))))))
 
-  clojure.lang.ISeq
-  (eval-cps [[op :as sexp] env k]
-    (case op
-      ifte 
-      (let [[_ test-expr then else] sexp] 
-        (eval-cps test-expr
-                  env
-                  (fn [b]
-                    (if (boolean? b)
-                      (eval-cps (if b then else) env k)
-                      (top-k (format "%s not a boolean" b))))))
+(defn analyze-fn [sexp]
+  (let [[_ args & body] sexp]
+    (fn [env k] 
+      (k {:params args :body body :env env}))))
 
-      call-cc (let [[_ f] sexp]
-                (eval-cps (list f (Continuation. env k)) env k))
+(defn analyze-call-cc [sexp]
+  (let [[_ f] sexp]
+    (fn [env k] 
+      (eval-cps (list f (Continuation. env k)) env k))))
 
-      fn (let [[_ args & body] sexp]
-           (k {:params args :body body :env env}))
-
-      (let [[op & args] sexp] 
-        (eval-cps 
-          op 
-          env
-          (fn [p]
-            (println :p p)
-            (get-args 
-              args 
-              env 
-              (fn [as]
-                (cond 
-                  (primitive-fn? p) (k (apply p as))
-                  (instance? Continuation p)
-                  ((.k p) (first as))
-                  :else (eval-seq (:body p)
+(defn analyze-application [[op & args]]
+  (let [pf (analyze op)
+        args-fn (map analyze args)]
+    (fn [env k]
+      (pf
+        env
+        (fn [p]
+          (get-args 
+            args-fn
+            env 
+            (fn [as]
+              (cond 
+                (primitive-fn? p) (k (apply p as))
+                (instance? Continuation p)
+                ((.k p) (first as))
+                :else (trampoline eval-seq (:body p)
                                   (->IEnv (zipmap (:params p) as) 
                                           (:env p))
-                                  k))))))))))
+                                  k)))))))))
+
+#trace
+(extend-protocol IAnalyze
+  nil
+  (analyze [_] (fn [_ k] (k nil)))
+
+  java.lang.Long
+  (analyze [this] 
+    (analyze-self-eval this))
+
+  java.lang.Boolean
+  (analyze [this] 
+    (analyze-self-eval this))
+
+  clojure.lang.Symbol
+  (analyze [this]
+    (analyze-lookup this))
+
+  Continuation
+  (analyze [this]
+    (fn [_ k] (k this)))
+
+  clojure.lang.ISeq
+  (analyze [[op :as sexp]]
+    (case op
+      ifte (analyze-if sexp)
+      call-cc (analyze-call-cc sexp)
+      fn (analyze-fn sexp)
+      (analyze-application sexp))))
+
+#trace
+(defn eval-cps [sexp env k]
+  (fn [] ((analyze sexp) env k)))
 
 (defn run [exp]
-  (eval-cps exp top-env identity))
+  (trampoline eval-cps exp top-env identity))
 
 (comment 
-  (lookup-env-k top-env 'x identity #(println :fuck!))
+  (run '(+ 1 (+ 2 (+ 3 (+ 4 (+ 5 6))))))
 
   (run '(call-cc (fn [k0]
                    (+ 1
@@ -128,4 +166,7 @@
   (require '[flow-storm.api :as fs-api])
   (fs-api/local-connect)
   (fs-api/stop)
+
+  #_(require '[clj-java-decompiler.core :refer [decompile]])
+
   ,)
